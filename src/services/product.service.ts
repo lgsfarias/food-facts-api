e6133b0486b98f936221e14cce1b357ccf051b12
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-10-31 21:05:59 -0300

    feat: create product service

---
+++ src/services/product.service.ts
@@ -0,0 +1,39 @@
+import { Product } from '../interfaces';
+import { ProductRepository } from '../repositories/product.repository';
+import AppError from '../utils/AppError';
+
+export class ProductService {
+  productRepository: ProductRepository;
+
+  constructor() {
+    this.productRepository = new ProductRepository();
+  }
+
+  public async findAll(page: number, limit: number): Promise<Product[]> {
+    return await this.productRepository.findAll(page, limit);
+  }
+
+  public async findOne(code: number): Promise<Product> {
+    const productFound = await this.productRepository.findOne(code);
+    if (!productFound) {
+      throw new AppError('Product not found', 404);
+    }
+    return productFound;
+  }
+
+  public async update(code: number, product: Product): Promise<Product | null> {
+    const productFound = await this.productRepository.findOne(code);
+    if (!productFound) {
+      throw new AppError('Product not found', 404);
+    }
+    return await this.productRepository.update(code, product);
+  }
+
+  public async delete(code: number): Promise<Product | null> {
+    const productFound = await this.productRepository.findOne(code);
+    if (!productFound) {
+      throw new AppError('Product not found', 404);
+    }
+    return await this.productRepository.delete(code);
+  }
+}
